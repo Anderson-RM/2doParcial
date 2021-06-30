0d5a0127941596a0feb51651c5b29e1e59fe58ce
Anderson Rodriguez Molina <anderson_rodriguez@ucne.edu.do>
2021-06-30 14:34:25 -0400

    Se limpio el area de trabajo y se agregaron las carpetas correspondientes.

--- Parcial2/Startup.cs
+++ Parcial2/Startup.cs
@@ -1,3 +1,4 @@
+using Blazored.Toast;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -28,7 +29,7 @@ namespace Parcial2
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-
+            services.AddBlazoredToast();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
