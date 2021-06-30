9caa382f56668918a9bdc9734f1abe0871c2abb7
Anderson Rodriguez Molina <anderson_rodriguez@ucne.edu.do>
2021-06-30 19:11:32 -0400

    Editando el contexto

--- Parcial2/DAL/Contexto.cs
+++ Parcial2/DAL/Contexto.cs
@@ -17,7 +17,7 @@ namespace Parcial2.DAL
         public DbSet<Clientes> Clientes { get; set; }        
         public DbSet<Ventas> Ventas { get; set; }
         public DbSet<Cobros> Cobros { get; set; }
-        /*
+        
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Clientes>().HasData(new Clientes() { ClienteId = 1, Nombres = "FERRETERIA GAMA" });
@@ -31,6 +31,6 @@ namespace Parcial2.DAL
             modelBuilder.Entity<Clientes>().HasData(new Clientes() { ClienteId = 3, Nombres = "PRESTAMOS CEFIPROD" });
             modelBuilder.Entity<Ventas>().HasData(new Ventas() { VentaId = 5, Fecha = new DateTime(2020, 09, 01), ClienteId = 3, Monto = 3000, Balance = 3000 });
             modelBuilder.Entity<Ventas>().HasData(new Ventas() { VentaId = 6, Fecha = new DateTime(2020, 10, 01), ClienteId = 3, Monto = 2900, Balance = 1900 });
-        }*/
+        }
     }
 }
